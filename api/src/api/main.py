3a26f051da492ec16981d22ee358b2832977fbcb
Tomáš Raček <tom@krab1k.net>
2025-11-24 07:32:39 +0100

    Change contact

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 
 app = FastAPI(title='ChannelsDB 2.0 API',
-              contact={'name': 'Tomáš Raček', 'email': 'tomas.racek@ceitec.muni.cz'},
+              contact={'name': 'Tomáš Raček', 'email': 'tomas.racek@muni.cz'},
               redoc_url=None, docs_url='/',
-              version='beta',
+              version='1.0.0',
               swagger_ui_parameters={'syntaxHighlight': False, 'defaultModelsExpandDepth': -1})
